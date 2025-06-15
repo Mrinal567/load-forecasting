53b3d04f8d32900aadf560d36c38f31bbab011fa
Mrinal567 <mrinal098sh@gmail.com>
2025-06-15 09:56:33 +0600

    Update model.py

--- model.py
+++ model.py
@@ -4,10 +4,10 @@ import joblib
 import os
 
 MODEL_DIR = os.path.join(os.path.dirname(__file__), "models")  # Get the directory of the current file
-lstm_model_hr_path = os.path.join(MODEL_DIR, "lstm_model.h5")
-lstm_model_day_path = os.path.join(MODEL_DIR, "lstm_model_1_day.h5")
-scaler_hr_path = os.path.join(MODEL_DIR, "scaler_1.pkl")
-scaler_day_path = os.path.join(MODEL_DIR, "scaler (3).pkl")
+lstm_model_hr_path = os.path.join(MODEL_DIR, "1_hour.h5")
+lstm_model_day_path = os.path.join(MODEL_DIR, "1_day.h5")
+scaler_hr_path = os.path.join(MODEL_DIR, "scaler_hour.pkl")
+scaler_day_path = os.path.join(MODEL_DIR, "scaler_day.pkl")
 
 lstm_model_hr = load_model(lstm_model_hr_path)
 lstm_model_day = load_model(lstm_model_day_path)
